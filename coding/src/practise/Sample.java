18e5923906ef27198c65c19971f58863ac3a2304
sobhitha <sobhithamallapu009@gmail.com>
2019-03-05 20:04:20 +0530

    sample java program

---
+++ coding/src/practise/Sample.java
@@ -0,0 +1,18 @@
+package practise;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello world");
+			int [] arr = new int [3];
+			arr[0]=1;
+			arr[1]=2;
+			arr[2]=3;
+			for (int x: arr)
+			{
+				System.out.println(x);
+			}
+	}
+
+}
