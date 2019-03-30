40a2b5cfb896493ff8ec9538f912a778c326b46d
sobhitha <sobhithamallapu009@gmail.com>
2019-03-30 11:01:13 +0530

    java operators

---
+++ JavaConcepts/Keywords&exprs/src/com/Sobhitha/Main.java
@@ -0,0 +1,8 @@
+package com.Sobhitha;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ JavaConcepts/Operators/src/com/Sobhitha/assigope.java
@@ -0,0 +1,19 @@
+package com.Sobhitha;
+
+public class assigope {
+    public static void  main (String[] args) {
+
+        double a = 20d;
+        double b = 80d;
+        double c = (    a + b) * 25;
+        System.out.println("value for #3 is " + c);
+
+        double d = c % 40;
+        System.out.println("value for #4 is " + d);
+
+        if(d <= 20)
+            System.out.println("Total was over the limit");
+
+
+    }
+}
