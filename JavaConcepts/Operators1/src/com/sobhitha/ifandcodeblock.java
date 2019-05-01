245725bb1bbaf31c1a640cb5d5ae46971503e93f
sobhitha <sobhithamallapu009@gmail.com>
2019-05-01 13:00:31 +0530

    java loops

--- JavaConcepts/Operators1/src/com/sobhitha/ifandcodeblock.java
+++ JavaConcepts/Operators1/src/com/sobhitha/ifandcodeblock.java
@@ -1,22 +1,48 @@
 package com.sobhitha;
 
 public class ifandcodeblock
-{        public static void main(String[] args)
-{
-
-            calucalateScore(true,800, 5, 100);
-
-            calucalateScore(true,10000, 8, 200);
+{        public static void main(String[] args) {
 
 
-            }
+    int highScorePosition = calucalateHighScorePosition(1500);
+    displayHighScorePosition("Twinkle", highScorePosition);
+
+    highScorePosition = calucalateHighScorePosition(900);
+    displayHighScorePosition("sobhitha", highScorePosition);
 
 
-        public static void calucalateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    highScorePosition = calucalateHighScorePosition(400);
+    displayHighScorePosition("Rinky", highScorePosition);
 
-                       if (gameOver) {
-                int finalScore = score + (levelCompleted * bonus);
-                System.out.println("Your final score is " + finalScore);
-            }
-        }
+    highScorePosition = calucalateHighScorePosition(50);
+    displayHighScorePosition("vicky", highScorePosition);
+
+    highScorePosition = calucalateHighScorePosition(1000);
+    displayHighScorePosition("vicky", highScorePosition);
 }
+
+
+
+
+        public static void displayHighScorePosition(String playerName , int highScorePosition)
+        {
+            System.out.println(playerName + " managed to get into position " +
+                    highScorePosition + " on the high score table");
+
+        }
+
+        public static int calucalateHighScorePosition(int playerScore) {
+            if (playerScore > 1000)
+                return 1;
+            else if (playerScore >= 500 && playerScore < 1000)
+                return 2;
+            else if (playerScore >= 100 && playerScore < 500)
+                return 3;
+            else
+                return 4;
+
+        }
+
+        }
+
+
